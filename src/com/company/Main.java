c3372f99d2a36a03bb6e2079ad837c86d62730c9
AlexPro <sawa.panteleimon.73@gmail.com>
2022-11-06 20:52:01 +0200

    Added initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello, my name is Alex!");
+	System.out.print("Hello, my name is Alex!!");
     }
 }
 
